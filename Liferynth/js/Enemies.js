2c12259f7907da07052fca384e274ce7090f48bc
YamanduSotelo <yamandu_leandro@hotmail.com>
2015-02-06 19:39:49 +0100

    Separación Enemies/Enemy
    - La clase Enemies (Enemies.js) se encargará de llevar la lista de enemigos y manejarlos.
    - La clase Enemy definirá a un enemigo concreto (tamaño, imagen...) así como su comportamiento.

---
+++ Liferynth/js/Enemies.js
@@ -0,0 +1,55 @@
+﻿Enemies = function (game) {
+
+    //Variables del juego que necesitamos aquí
+        this.game = game;
+        this.scene = game.scene;
+        this.walls = game.walls;
+        this.paintedWalls = game.paintedWalls;
+        this.posMatrix = game.posMatrix;
+        this.numWalls = game.numWalls;
+        this.rows = game.rows;
+        this.cols = game.cols;
+        this.floorHeight = game.floorHeight;
+        this.meshPlayer = game.meshPlayer;
+        this.collidingBox = game.collidingBox;
+        this.gravity = game.gravity;
+
+        this.enemies = game.enemies;
+        this.numEnemies = game.numEnemies;
+
+
+    this.CreateEnemy = function (row, col) {
+        /*Crea un enemigo en la posición row,col de la matriz de posiciones del laberinto*/
+        var e = new Enemy(game);
+        e.walls = walls;
+        var testPos = paintedWalls[lastRow][5].position; //TODO
+        e.position = new BABYLON.Vector3(testPos.x, testPos.y, testPos.z - 3);
+        e.move = function (horizontal, positive) {
+            /* 
+                Mueve al enemigo en una dirección dentro del laberinto
+            */
+            //Si (horizontal == true) --> Eje x
+            if (horizontal) {
+                if (positive) { e.position.x += wallWidth; }
+                else { e.position.x -= wallWidth; }
+            } else {
+                if (positive) { e.position.z += wallWidth; }
+                else { e.position.z -= wallWidth; }
+            }
+        };
+        enemies[numEnemies] = e;
+        numEnemies++;
+    }
+
+
+
+    this.ManageEnemies = function () {
+        //PARALELO
+        // No se modifica el mapa de muros, si un enemigo lo modificara, 
+        // se llevaría a cabo al finalizar el bucle (Tareas pendientes)
+        for (var i = 0; i < numEnemies; i++) {
+            enemies[i];
+        }
+
+    }
+}
--- Liferynth/js/Enemy.js
+++ Liferynth/js/Enemy.js
@@ -14,8 +14,6 @@
         this.collidingBox = game.collidingBox;
         this.gravity = game.gravity;
 
-        this.enemy = game.enemy;
-        this.numEnemies = game.numEnemies;
 
     // Mismo tamaño que el jugador por ahora (diversos enemigos, diversos tamaños) //TODO
     enemyHeight = 1.75;
@@ -23,39 +21,4 @@
     enemyLength = 0.5;
     ellipsoidEnemy = new BABYLON.Vector3(0.5, playerHeight / 4, 0.5); //Colisionador del enemigo
 
-
-    this.CreateEnemy = function (row, col) {
-        /*Crea un enemigo en la posición row,col de la matriz de posiciones del laberinto*/
-        var e = new Object();
-        e.walls = walls;
-        var testPos = paintedWalls[lastRow][5].position; //TODO
-        e.position = new BABYLON.Vector3(testPos.x, testPos.y, testPos.z - 3);
-        e.move = function (horizontal, positive) {
-            /* 
-                Mueve al enemigo en una dirección dentro del laberinto
-            */
-            //Si (horizontal == true) --> Eje x
-            if (horizontal) {
-                if (positive) { e.position.x += wallWidth; }
-                else { e.position.x -= wallWidth; }
-            } else {
-                if (positive) { e.position.z += wallWidth; }
-                else { e.position.z -= wallWidth; }
-            }
-        };
-        enemy[numEnemies] = e;
-        numEnemies++;
-    }
-
-
-
-    this.ManageEnemies = function () {
-        //PARALELO
-        // No se modifica el mapa de muros, si un enemigo lo modificara, 
-        // se llevaría a cabo al finalizar el bucle (Tareas pendientes)
-        for (var i = 0; i < numEnemies; i++) {
-            enemy[i];
-        }
-
-    }
 }
