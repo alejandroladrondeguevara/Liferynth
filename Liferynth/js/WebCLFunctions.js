5ee2190afab3fac0a1b3d05348d93f6891c7b12a
YamanduSotelo <yamandu_leandro@hotmail.com>
2015-02-17 13:45:20 +0100

    Adición de WebCLFunctions.
    - Función listDevices(platform) que muestra los dispositivos existentes en la plataforma correspondiente por consola.

---
+++ Liferynth/js/WebCLFunctions.js
@@ -0,0 +1,14 @@
+﻿
+
+function listDevices(platforms) {
+
+    for (var i in platforms) {
+        var plat = platforms[i];
+        var nameP = plat.getInfo(WebCL.PLATFORM_NAME);
+        var devices = plat.getDevices();
+        for (var j in devices) {
+            var nameD = devices[j].getInfo(WebCL.DEVICE_NAME);
+            console.log('Platform: ', nameP, 'Device: ', nameD);
+        }
+    }
+}
